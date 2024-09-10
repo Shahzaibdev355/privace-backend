05abdbb169bd6a287d6a159eed10bf05a922193a
Shahzaib_Dev <135271813+Shahzaibdev355@users.noreply.github.com>
2024-09-11 02:36:04 +0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -7,16 +7,7 @@ const router = express.Router()
 
 
 // Route handler
-// app.post("/charge", handlePayment);
 
-
-// app.post("/booknow",handleBooking);
-
-
-// app.post("/contactus", handleContact)
-
-
-// Define your API routes using router instead of app
 router.post("/charge", handlePayment);
 router.post("/booknow", handleBooking);
 router.post("/contactus", handleContact);
